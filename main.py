396e6cc3a0694363d6e9b637b0e977506aef1b82
mirazizk <miraziz2008@yandex.com>
2025-12-08 15:44:59 +0500

    main2

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ class Product:
         self.price = price
         self.qty = qty
 
+    def get_info(self):
+        return f"{self.name} - {self.price} so`m (qolgan: {self.qty} ta)"
+
+
 class Catalog:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def get_info(self):
+        print("\nRadius - Mahsulotlar:")
+        for i in self.products:
+            print(i.get_info())
+
+
 class Order:
     orders = []
     def __init__(self, product, qty):
@@ -21,21 +34,30 @@ class Order:
         self.qty = qty
         Order.orders.append(self)
 
+catalog = Catalog()
+catalog.add(Product("olma", 10000, 2000))
+catalog.add(Product("nok", 15000, 3000))
+catalog.add(Product("guruch", 5000, 5000))
 
 
-product1 = Product("olma", 10000, 2000)
-product2 = Product("nok", 15000, 3000)
-product3 = Product("guruch", 15000, 5000)
+def menu():
+    while True:
+        a = input("1.productlar\n2.savat\n3.exit\n>>> ")
 
-c1 = Catalog("1-catolog")
-c1.products = [product1, product2, product3]
-# 1
-for i in c1.products:
-    print(f'{i.name}-{i.price}-{i.qty}')
-# 2.productni rasmiylashtirish
-or1 = Order(product1, 20)
-or2 = Order(product2, 10)
-or3 = Order(product3, 30)
+        if a == "1":
+            b = input("produkt qoshamizmi (ha yoki yo`q): ")
+            if b == "ha":
+                catalog.get_info()   # ❗ print bilan emas!
+            elif b == "":
+                catalog.get_info()
+                menu()
+            elif b == "exit":
+                pass
+        elif a == "2":
+            print("savat bo‘sh")
+            menu()
 
-for i in Order.orders:
-    print(f'{i.product.name}-{i.qty}')
+        elif a == "3":
+            print("chiqish")
+            break
+menu()
