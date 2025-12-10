76e407572d84ad68310f353b307eda05f7370dd7
mirazizk <miraziz2008@yandex.com>
2025-12-10 18:32:02 +0500

    catalog_finsh1

--- catalog.py
+++ catalog.py
@@ -1,4 +1,11 @@
 class Catalog:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def get_info(self):
+        print("\nRadius - Mahsulotlar:")
+        for i in self.products:
+            print(i.get_info())
